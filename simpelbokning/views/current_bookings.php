4bf5b7c1f85d12e3694fe444a3121c53cc1b73eb
Johan Rylander <johan.rylander@svt.se>
2024-09-01 20:17:10 +0200

    Show three weeks

--- simpelbokning/views/current_bookings.php
+++ simpelbokning/views/current_bookings.php
@@ -10,29 +10,41 @@ $all_bookings = $wpdb->get_results("SELECT * FROM $table_name");
 <?php
 $first_slot_hour = \get_option('simpelbokning_first_slot_hour');
 $last_slot_hour = \get_option('simpelbokning_last_slot_hour');
-// $now = new \DateTimeImmutable();
-// $week = $now->format('W');
-// $year = $now->format('Y');
-// $week_start = $now->setISODate($year, $week)->setTime($first_slot_hour, 0, 0);
+$now = new \DateTimeImmutable();
+$this_week = $now->format('W');
+$year = $now->format('Y');
 // echo $week_start->format('Y-m-d H:i:s');
 ?>
 <div id="simpelbokning">Aktiva bokningar:
+<?php for ($week = 0; $week <= 2; $week++) {?>
     <table>
         <tr>
-            <th><?=__('Monday', 'simpelbokning')?></th>
-            <th><?=__('Tuesday', 'simpelbokning')?></th>
-            <th><?=__('Wednesday', 'simpelbokning')?></th>
-            <th><?=__('Thursday', 'simpelbokning')?></th>
-            <th><?=__('Friday', 'simpelbokning')?></th>
-            <th><?=__('Saturday', 'simpelbokning')?></th>
-            <th><?=__('Sunday', 'simpelbokning')?></th>
+            <th><?=__('w', 'simpelbokning')?><?= $this_week + $week ?></th>
+            <th style="width: 6em";><?=__('Monday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Tuesday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Wednesday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Thursday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Friday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Saturday', 'simpelbokning')?></th>
+            <th style="width: 6em";><?=__('Sunday', 'simpelbokning')?></th>
         </tr>
         <?php for ($hour = $first_slot_hour; $hour <= $last_slot_hour; $hour++) {?>
         <tr>
-            <?php for ($weekday = 0; $weekday <= 6; $weekday++) {?>
-                <td><?=$hour?></td>
-            <?php }?>
+            <td><?=$hour?></td>
+            <?php
+                for ($day = 1; $day <= 7; $day++) {
+                    $this_hour = $now->setISODate($year, $this_week+$week, $day)->setTime($hour, 0, 0);
+            ?>
+                <td style="border: 1px solid black;">
+                    <?php
+                    if ($this_hour->getTimestamp() < $now->getTimestamp()) {
+                            echo '-';
+                        }
+                    ?>
+                </td>
+            <?php } // day ?>
         </tr>
-        <?php }?>
+        <?php } // hour ?>
     </table>
+<?php } // week ?>
 </div>
