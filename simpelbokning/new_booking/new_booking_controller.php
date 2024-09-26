a899411e0049405643259db0bd7bf5153967ca54
Johan Rylander <johan.rylander@svt.se>
2024-09-26 15:36:02 +0200

    Check for empty booking date

--- simpelbokning/new_booking/new_booking_controller.php
+++ simpelbokning/new_booking/new_booking_controller.php
@@ -6,13 +6,17 @@ require_once dirname(__FILE__) . '/../utils.php';
 
 function new_booking()
 {
-    $slot_start_as_epoch = $_POST['slot_start'];
-    \error_log("slot_start_as_epoch: $slot_start_as_epoch");
-    $slot_start = new \DateTime("@$slot_start_as_epoch");
-    
-    if (!is_bookable($slot_start_as_epoch)) {
-        require_once dirname(__FILE__) . '/new_booking_non_bookble_view.php';
-    } else {
-        require_once dirname(__FILE__) . '/new_booking_view.php';
+    if (!empty($_POST['slot_start'])) {
+        $slot_start_as_epoch = $_POST['slot_start'];
+
+        // Model for the views
+        $slot_start = new \DateTime("@$slot_start_as_epoch");
+
+        if (!is_bookable($slot_start_as_epoch)) {
+            require_once dirname(__FILE__) . '/new_booking_non_bookable_view.php';
+        } else {
+            require_once dirname(__FILE__) . '/new_booking_view.php';
+        }
+
     }
 }
