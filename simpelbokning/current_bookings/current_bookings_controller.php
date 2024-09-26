c687889a2afad59a1ddc8feb71d7acf5de9b32b5
Johan Rylander <johan.rylander@svt.se>
2024-09-26 15:29:25 +0200

    Use prepared statement for selection of current bookings

--- simpelbokning/current_bookings/current_bookings_controller.php
+++ simpelbokning/current_bookings/current_bookings_controller.php
@@ -7,7 +7,8 @@ function current_bookings()
     global $wpdb;
 
     $table_name = $wpdb->prefix . 'simpelbokning_bookings';
-    $query = "SELECT time, length_minutes, name FROM $table_name WHERE time BETWEEN NOW() AND NOW() + INTERVAL " . \get_option('simpelbokning_weeks_to_show') . " WEEK;";
+
+    $query = $wpdb->prepare("SELECT time, length_minutes, name FROM $table_name WHERE time BETWEEN NOW() AND NOW() + INTERVAL %d WEEK;", \get_option('simpelbokning_weeks_to_show'));
     $current_bookings = $wpdb->get_results($query);
 
     $first_slot_hour = \get_option('simpelbokning_first_slot_hour');
